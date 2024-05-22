75b6d3dc041064316e6f4758c7a80ca4c45ddfe0
FilippFlip <f.mishenev@gmail.com>
2024-05-22 19:27:02 +0300

    First Commit

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public bool isFriendly;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (isFriendly)
+        {
+          transform.position = transform.position + Vector3.right * Time.deltaTime;
+
+        }
+        else
+        {
+            transform.position = transform.position + Vector3.left * Time.deltaTime;
+        }
+       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Unit>(out Unit fight))
+        {
+            if (isFriendly == fight.isFriendly)
+            {
+                ///////////////////Анимация атаки будет здесь///////////////////////////////////
+            }
+        }
+    }
+
+}
