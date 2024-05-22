d5b8c2721b906e42c05efdd36ec8132e5556e966
FilippFlip <f.mishenev@gmail.com>
2024-05-22 19:55:38 +0300

    Clash-Mini

---
+++ Assets/Scripts/UnitStats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public int maxHP;
+    public int damage;
+    public float attackSpeed; 
+    public float speed;
+    public float lifeTime;
+    public int cost;
+    public int manacost;
+    public int countOfUnits;
+   
+}
